ea7518f036e1f2bb82cbff5e773bb044cb5a8fa9
Zeeshan Nasir <m.zeeshannasir5@gmail.com>
2023-01-30 00:22:29 +0500

    title modified

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function HomePage() {
   return (
     <React.Fragment>
       <Head>
-        <title>Home Page</title>
+        <title>evaly - Ecommerce</title>
         <meta name='description' content='Home Page' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
